30eaa303ff5ec3ff49b768b3fa9600b7f93c6db4
sknop8 <sknop8@gmail.com>
2016-04-02 15:26:23 -0400

    startTime and play

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
-function startTime() {
-	
+var GAME_TIME = 0;
+
+var startTime = function startTime() {
+	GAME_TIME++; 
+	document.getElementById('timer').innerHTML = GAME_TIME;
+}
+
+function play() {
+	startTime();
+	setTimeout(startTime, 1000);
 }
