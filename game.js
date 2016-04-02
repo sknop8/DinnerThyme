e484d3f333cd2957ceb77efea6e20ad1e40b88ca
Sally Kong <kongsally@users.noreply.github.com>
2016-04-02 16:11:52 -0400

    Updated style

--- game.js
+++ game.js
@@ -1,17 +1,44 @@
 var GAME_TIME = 0;
+var c, ctx;
 
 var timer = function timer() {
 	GAME_TIME++; 
-	document.getElementById('timer').innerHTML = GAME_TIME;
+	document.getElementById('thymer').innerHTML = GAME_TIME;
 };
 
 
-var close_startScreen = function close_startScreen() {
+var closeStartScreen = function closeStartScreen() {
 	$("#title").css("display", "none");
 	$("#play_button").css("display", "none");
 };
 
+var openGameScreen = function openGameScreen() {
+
+	var WIDTH = window.innerWidth,
+      	HEIGHT = window.innerHeight;
+
+     c = $("#canvas"); 
+     ctx = c[0].getContext('2d');
+
+     ctx.canvas.width = WIDTH;
+     ctx.canvas.height = HEIGHT;
+
+	$("#canvas").css("display", "block");
+
+	make_thyme();
+}
+
 var play  = function play() {
-	startTime();
+	closeStartScreen();
+	openGameScreen();
+	timer();
 	setInterval(timer, 1000);
+};
+
+var make_thyme = function make_thyme() {
+	
+};
+
+var change_thyme = function change_thyme() {
+
 };
